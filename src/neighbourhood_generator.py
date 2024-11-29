9aab42d9f23a7268ab5c6ac20c66aac630a72ad6
Wolfxyz16 <yerayliloaiza@gmail.com>
2024-11-29 17:09:26 +0100

    random_neighborhood created but nor working

--- src/neighbourhood_generator.py
+++ src/neighbourhood_generator.py
@@ -20,21 +20,43 @@ class NeighbourhoodGenerator:
     Returns:
         neighbourhood(Neighbourhood): Instancia de tipo Neighbourhood de la selección de los ids de los jugadores que forman parte de la solución.
     """
-
     neighbourhood = Neighbourhood(solution)
 
     # Por cada jugador en la solución actual.
     for current_player in solution.lineup:
-
-        # Recorremos los ids de los jugadores hasta el final.
-        for id in range( current_player["id"] + 1, problem.total_players ):
-            new_player = problem.players[id]
+        # Recorremos los ids de los jugadores desde el current_player hasta el final.
+        for id, new_player in enumerate(problem.total_players, start = current_player["id"]):
             if current_player["position"] == new_player["position"] and new_player not in solution.lineup:
                 # Hemos encontrado otro jugador con la misma posición
                 new_solution = solution.copy()
-                new_solution[i] = players[j]["id"]
+                new_solution[id] = new_player
                 break
             
         neighbourhood.append(new_solution)
 
     return neighbourhood
+
+    # NO FUNCIONA
+    def random_neighborhood(self, solution, neighborhood_generator):
+    """
+    Genera once soluciones cogiendo un jugador aleatorio e insertando en cada elemento del vector el jugador.
+
+    Args:
+        Players(list): Lista de todos los jugadores de nuestro dataset
+        Solution(list): Lista de un estado solución actual con los id de todos los jugadores
+
+    Returns:
+        neighborhoods(List): Lista de once soluciones. Una por cada inserción.
+    """
+    encontrado = False
+    neighboors = []
+    no_visitados = neighborhood_generator.no_visitados.copy()
+    idx = random.randint(0, len(no_visitados)-1)
+    new_player = neighborhood_generator.players[idx]['id']
+    neighborhood_generator.borrar_elemento_visitado(idx)
+    for i in range(len(solution)):
+        new_solution = solution.copy()
+        new_solution[i] = new_player
+        neighboors.append(new_solution)
+
+    return neighboors
