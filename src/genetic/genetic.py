4d468bf18e4ec13c8b4df3882d0d0b3e3478ecfe
Wolfxyz16 <yerayliloaiza@gmail.com>
2024-12-11 18:22:42 +0100

    empezamos a escribir el algoritmo genetico

---
+++ src/genetic/genetic.py
@@ -0,0 +1,30 @@
+# src/genetic/genetic.py
+
+from src.problem import Problem
+
+# Creamos una poblacion inicial y las evaluamos
+
+# iteramos hasta un criterio
+while True:
+    while hijos < 10:
+        # 1. Seleccionamos a dos padres
+        # 2. Aplicamos el cruce
+        # 3. Aplicamos la mutación
+        # 4. Insertamos los nuevos hijos en la poblacion
+        pass
+
+    # Evaluamos la nueva poblacion
+
+    # Seleccionamos la mejor poblacion
+
+    # Devolvemos el mejor individuo
+
+class population:
+    """
+    Representa un conjunto de soluciones
+    """
+
+    def __init__(self):
+        self.
+        pass
+    pass
